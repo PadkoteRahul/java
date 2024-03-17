0164023ae4179982798010bf7e2eddefe0cf8ce1
PadkoteRahul <padkoterahul1234@gmail.com>
2024-03-17 10:41:50 +0530

    types of making streams works

--- java8/streams/Streams.java
+++ java8/streams/Streams.java
@@ -6,25 +6,27 @@ public class Streams {
     public static void main(String[] args) {
         List<Integer> l = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
 
-        // Stream<Integer> s1 = l.stream();
-        // Stream<Integer> s2 = s1.filter(n->n%2 != 0);
-        // Stream<Integer> s3 = s2.map(n->n*2);
-        // // s3.forEach(i->System.out.println(i));
-        // int result = s3.reduce(0,(c,e)->c+e);
+        Stream<Integer> s1 = l.stream();
+        Stream<Integer> s2 = s1.filter(n->n%2 != 0);
+        Stream<Integer> s3 = s2.map(n->n*2);
+        // s3.forEach(i->System.out.println(i));
+        int result = s3.reduce(0,(c,e)->c+e);
+        System.out.println(result);
 
 
-        // Stream<Integer> s1  = l.stream()
-        //             .filter(n -> n%2 != 0)
-        //             .map(n->n*2);
-        // // s1.forEach(n->System.out.println(n));
-        // int result = s1.reduce(0, (c,e)->c+e);
+        Stream<Integer> s4  = l.stream()
+                    .filter(n -> n%2 != 0)
+                    .map(n->n*2);
+        // s4.forEach(n->System.out.println(n));
+        int result1 = s4.reduce(0, (c,e)->c+e);
+        System.out.println(result1);
 
 
-        int result = l.stream()
+        int result2 = l.stream()
                     .filter(n -> n%2 != 0)
                     .map(n->n*2)
                     .reduce(0, (c,e)->c+e);
 
-        System.out.println(result);
+        System.out.println(result2);
     }
 }
