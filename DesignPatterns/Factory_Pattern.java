438afa4c353fe232421a853c85a92861a7fcd31f
PadkoteRahul <padkoterahul1234@gmail.com>
2024-03-23 06:29:32 +0530

    Factory pattern updates

--- DesignPatterns/Factory_Pattern.java
+++ DesignPatterns/Factory_Pattern.java
@@ -1,3 +1,19 @@
+// The Factory Pattern is a creational design pattern that provides an interface for 
+// creating objects in a superclass but allows subclasses to alter the type of objects that 
+// will be created. This pattern promotes loose coupling by decoupling the instantiation of 
+// objects from their usage.
+
+//steps to create the factory pattern
+// ========================================================
+// 1.Define the Factory Interface (ButtonFactory)
+// 2.Implement Concrete Factories:(WindowsButtonFactory, IosButtonFactory)
+// 3.Define Product Interface:(Button)
+// 4.Implement Concrete Products:(WindowsButton, IosButton)
+//     Client Utilizes the Factory:
+
+//     The client code interacts with the factory to create objects without knowing the 
+//         specific class of the object being created.
+// ==============================================
 interface Button{
     void render();
     void Onclick();
